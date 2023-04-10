a7c465f32563137f0817dfbd9bd4981a6afe0d8d
TimoHenderson <tjhenderson2@gmail.com>
2023-04-10 17:11:48 +0100

    Printer has Toner, uses toner and will not print if there is not enough

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,12 +1,27 @@
 public class Printer {
 
     private int paper;
-    public Printer(int paper) {
+
+    private int toner;
+
+    public Printer(int paper, int toner) {
         this.paper = paper;
+        this.toner = toner;
     }
 
     public int getPaper() {
         return paper;
     }
 
+    public int getToner() {
+        return toner;
+    }
+
+    public void print(int pages, int copies) {
+        int paperRequired = pages * copies;
+        if (this.paper >= paperRequired && this.toner >= paperRequired){
+            this.paper -= paperRequired;
+            this.toner -= paperRequired;
+        }
+    }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -8,12 +8,42 @@ public class PrinterTest {
     Printer printer;
     @Before
     public void setUp(){
-        printer = new Printer(20);
+        printer = new Printer(20,18);
     }
 
     @Test
     public void hasPaper(){
         assertEquals(20,printer.getPaper());
+    }
+
+    @Test
+    public void willPrintIfEnoughPaper(){
+        printer.print(3,4);
+        assertEquals(8,printer.getPaper());
+    }
+
+    @Test
+    public void willNotPrintIfNotEnoughPaper(){
+        printer.print(3,7);
+        assertEquals(20,printer.getPaper());
+    }
+
+    @Test
+    public void hasToner(){
+        assertEquals(18,printer.getToner());
+    }
+
+    @Test
+    public void usesToner(){
+        printer.print(1,7);
+        assertEquals(11,printer.getToner());
+    }
+
+    @Test
+    public void willNotPrintIfNotEnoughToner(){
+        Printer printer2 = new Printer(100,10);
+        printer2.print(8,2);
+        assertEquals(10, printer2.getToner());
     }
 
 }
